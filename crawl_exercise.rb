8a04c5590b98572e9cb091a873d654bc64f41dab
Purin Kozuke <p@spacepro.be>
2016-12-04 00:59:18 +0900

    :+1: nested_table 追加しました

--- crawl_exercise.rb
+++ crawl_exercise.rb
@@ -3,10 +3,21 @@ class CrawlExercise < App
     slim :'crawl/index'
   end
 
+  # https://github.com/hitode909/great-redirect-loop
+  get '/redirect_loop' do
+    redirect '/crawl/redirect_loop'
+  end
+
+  get '/redirect_loop/:number' do
+    redirect "/crawl/redirect_loop/#{params[:number].to_i + 1}"
+  end
+
+  get '/nested_table' do
+    slim :'crawl/nested_table'
+  end
+
   # http://www.town.higashikagura.lg.jp/live/live.php
   get '/stream' do
-    content_type :txt
-
     poem = %w(クソッタレの世界のため 全てのクズ共のために 僕や君や彼等のため 明日には笑えるように)
     stream do |out|
       0.upto(Float::INFINITY) do |i|
@@ -16,14 +27,5 @@ class CrawlExercise < App
         sleep 1
       end
     end
-  end
-
-  # https://github.com/hitode909/great-redirect-loop
-  get '/redirect_loop' do
-    redirect '/crawl/redirect_loop'
-  end
-
-  get '/redirect_loop/:number' do
-    redirect "/crawl/redirect_loop/#{params[:number].to_i + 1}"
   end
 end
